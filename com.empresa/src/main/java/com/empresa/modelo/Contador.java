2f0e9c199e3fdbb96185e4ea89bd6803f71a5f95
Rodrigo Herrera <151490329+Rodrigosmurf27@users.noreply.github.com>
2025-02-25 18:07:59 -0600

    Delete com.empresa directory

--- com.empresa/src/main/java/com/empresa/modelo/Contador.java
+++
@@ -1,7 +0,0 @@
-package com.empresa.modelo;
-
-public class Contador extends Empleado {
-    public Contador(String nombre, int id, double salario, String departamento) {
-        super(nombre, id, salario, departamento);
-    }
-}
